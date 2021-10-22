866be7c53b8d93f71612d26f17bfd3a5fd82a8d9
siljp24 <silvia_juarez_24@hotmail.com>
2021-10-22 10:23:00 +0200

    enlaces sign-in sign-up

--- components/RsAppBar.vue
+++ components/RsAppBar.vue
@@ -3,8 +3,8 @@
         <v-app-bar app dark> 
             <v-toolbat-title>Red Social</v-toolbat-title>
             <v-spacer></v-spacer>
-            <v-btn text class="mx-2">Sign In</v-btn>
-            <v-btn text>Sign Up</v-btn>
+            <v-btn text class="mx-2" to="/sign-in">Sign In</v-btn>
+            <v-btn text to="/sign-up">Sign Up</v-btn>
         </v-app-bar>
     </div>
 </template>
--- components/RsSignUpForm.vue
+++ components/RsSignUpForm.vue
@@ -5,11 +5,11 @@
                 <v-card>
                     <v-card-title class="justify-center">Sign Up</v-card-title>
                     <v-card-text>
-                        <v-file-input placeholder="Avatar" outlined prepend-icon=""></v-file-input>
-                        <v-text-field placeholder="Email" type="email" outlined></v-text-field>
-                        <v-text-field placeholder="Password" type="password" outlined></v-text-field>
-                        <v-text-field placeholder="Repeat Password" type="password" outlined></v-text-field>
-                        <v-btn color="success" block>Submit</v-btn>
+                        <v-file-input v-model="avatar" placeholder="Avatar" outlined prepend-icon="" name="avatar"></v-file-input>
+                        <v-text-field v-model="email" placeholder="Email" type="email" outlined name="email"></v-text-field>
+                        <v-text-field v-model="password" placeholder="Password" type="password" outlined name="password"></v-text-field>
+                        <v-text-field v-model="password2" placeholder="Repeat Password" type="password" outlined name="password2"></v-text-field>
+                        <v-btn color="success" block v-on:click="submit">Submit</v-btn>
                     </v-card-text>
                 </v-card>
             </v-col>
@@ -18,3 +18,45 @@
         
     </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            avatar:undefined,
+            email:'',
+            password:'',
+            password2:'',
+        }
+    },
+    methods:{
+        async submit(){
+            if(this.password !== this.password2){
+                alert('Contraseñas diferentes');
+                return;
+            };
+            const formData = new FormData();
+            formData.enctype = 'multipart/form-data';
+            formData.append('avatar', this.avatar);
+            formData.append('email', this.email);
+            formData.append('password', this.password);
+            try{
+                const res = await fetch('http://localhost:4500/api/user/signUp',{
+                    method: 'POST',
+                    body: formData,
+                })
+                const data = await res.json();
+                if(data.error){
+                    alert(data.error);
+                    return;
+                }else{
+                    this.$router.push('/sign-in');
+                }
+            }catch(err){
+                alert(err.message);
+            }
+
+        }
+    }
+}
+</script>
